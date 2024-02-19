927e6a46abc7e488ca5c79c7b7e62756dd64d92a
zhenhappy <q505507538@gmail.com>
2024-02-19 10:25:06 +0800

    feat: add Surge Module

--- wechat-video-download.js
+++ wechat-video-download.js
@@ -1,13 +1,13 @@
 try {
-  let body = $response.body
-  body = body.replace(
-    '</body>', `
+  $done({
+    body: $response.body.replace(
+      '</body>', `
         <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
         <script>eruda.init();</script>
         <script src="https://i.wantmake.love:8091/wechat-video-download/wechat-video-download.inject.js?t=${new Date().getTime()}"></script>
         </body>
     `)
-  $done({body})
+  })
 } catch (e) {
   $done({})
 }
